4c7dd190132fbad15c84314f99640f3f1538d108
Gabriel Perdue <perdue@fnal.gov>
2017-10-14 18:07:51 -0500

    some improvement, but also some profound confusion

--- app/hdf5_test.cxx
+++ app/hdf5_test.cxx
@@ -13,34 +13,29 @@ void fill_arr_img(std::array<std::array<std::array<float, 2>, 5>, 4>& xs) {
     for (int i = 0; i < 2; ++i) {
         for (int j = 0; j < 5; ++j) {
             for (int k = 0; k < 4; ++k) {
-                xs[i][j][k] = gRandom->Gaus(0.0, 1.0);
+                // xs[i][j][k] = gRandom->Gaus(0.0, 1.0);
+                xs[i][j][k] = i * 100.0 + float(j + k);
+                std::cout << i << " " << j << " " << k << " = " << xs[i][j][k] << std::endl;
             }
         }
     }
 }
 
-void fill_arr(std::array<float, 2>& arrs) {
+void rearrange_arr(const std::array<std::array<std::array<float, 2>, 5>, 4>& xs, std::array<float, 40>& ys) {
+   int indx = 0;
     for (int i = 0; i < 2; ++i) {
-        arrs[i] = gRandom->Gaus(0.0, 1.0);
-    }
-}
-
-void fill_arr2(std::array<std::array<float, 2>, 2>& arrs) {
-    for (int i = 0; i < 2; ++i) {
-        for (int j = 0; j < 2; ++j) {
-            arrs[i][j] = gRandom->Gaus(0.0, 1.0);
+        for (int j = 0; j < 5; ++j) {
+            for (int k = 0; k < 4; ++k) {
+                ys[indx] = xs[i][j][k];
+                std::cout << i << " " << j << " " << k << " = " << indx << " -> " << xs[i][j][k] << " = " << ys[indx] << std::endl;
+                // std::cout << i << " " << j << " " << k << " " << indx << std::endl;
+                // std::cout << xs[i][j][k] << std::endl;
+                indx += 1;
+            }
         }   
     }
+    std::cout << indx << std::endl;
 }
-
-void print_arr2(const std::array<std::array<float, 2>, 2>& arrs) {
-    for (int i = 0; i < 2; ++i) {
-        for (int j = 0; j < 2; ++j) {
-            std::cout << "arrs[" << i << "][" << j << "] = " << arrs[i][j] << std::endl;
-        }   
-    }
-}
-
 
 int main( int argc, char *argv[]) try {
 
@@ -51,28 +46,19 @@ int main( int argc, char *argv[]) try {
     unsigned char segs[] = {1, 2, 3};
     unsigned short int pcodes[] = {9, 18, 27};
     float zs[] = {4000.0, 4400.0, 4800.0};
-    std::array<float, 2> arrs = {0};
-    std::array<std::array<float, 2>, 2> arrs2 = {0.};
     std::array<std::array<std::array<float, 2>, 5>, 4> xs = {0.};
+    std::array<float, 40> ys = {0.};
 
     eventdat_nt_t eventdat(hdffile, "event_data", {"eventids", "segments", "planecodes", "zs"});
     imgdat_nt_t imgdat(hdffile, "img_data", {
             {"hitimes-x", {2, 5, 4}}
             });
-    arrdat_nt_t arrdat(hdffile, "arr_data", { {"arrdat", 2} });
-    // arr2dat_nt_t arr2dat(hdffile, "arr2_data", { {"arrdat", {2, 2}} });
-    auto arr2dat = make_ntuple({hdffile, "arr2_data"}, make_column<float, 2>("arrdat", {2, 2}));
+    // auto arr2dat = make_ntuple({hdffile, "arr2_data"}, make_column<float, 2>("arrdat", {2, 2}));
     for (int ev = 0; ev < 3; ++ev) {
         eventdat.insert(evtids[ev], segs[ev], pcodes[ev], zs[ev]);
-        fill_arr(arrs);
-        arrdat.insert(arrs.data());
-        std::cout << arrs.data() << std::endl;
-        fill_arr2(arrs2);
-        // arr2dat.insert(arrs2.data());
-        // std::cout << arrs2.data() << std::endl;
-        // print_arr2(arrs2);
         fill_arr_img(xs);
-        // imgdat.insert(xs.data());
+        rearrange_arr(xs, ys);
+        imgdat.insert(ys.data());
     }
 
     return 0;
--- include/NtupleTypes.h
+++ include/NtupleTypes.h
@@ -9,8 +9,6 @@ using eventdat_nt_t = hep_hpc::hdf5::Ntuple<
     unsigned long int, unsigned char, unsigned short int, float
     >;
 using imgdat_nt_t = hep_hpc::hdf5::Ntuple<hep_hpc::hdf5::Column<float, 3>>;
-using arr2dat_nt_t = hep_hpc::hdf5::Ntuple<hep_hpc::hdf5::Column<float, 2>>;
-using arrdat_nt_t = hep_hpc::hdf5::Ntuple<hep_hpc::hdf5::Column<float, 1>>;
 
 // using imgdat_nt_t = hep_hpc::hdf5::Ntuple<float, float, float>;
 // using imgdat_nt_t = hep_hpc::hdf5::Ntuple<
